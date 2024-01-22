b8ed865aa4890f03243b42e73a54205b23302405
ratankumarthakur <144756277+ratankumarthakur@users.noreply.github.com>
2024-01-22 10:13:04 +0530

    Create It's_widget.dart

---
+++ It's_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class language extends StatefulWidget {
+  const language({super.key});
+
+  @override
+  State<language> createState() => _languageState();
+}
+
+class _languageState extends State<language> {
+  int sel=0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: (){
+                setState(() {
+                  sel+=1;
+                });
+              },child: Text("Change the language"),
+            ),SizedBox(height: 20,),
+            //Text(sel?"Hello Ratan":"مرحبا راتان",style: TextStyle(color: Colors.red,fontSize: 40),)
+            if(sel==0)
+              Text("Hello Ratan",style: TextStyle(color: Colors.red,fontSize: 40),)
+            else if(sel==1)
+              Text("مرحبا راتان",style: TextStyle(color: Colors.red,fontSize: 40),)
+            else if(sel==2)
+                Text("olá ratan",style: TextStyle(color: Colors.red,fontSize: 40),)
+          ],
+        ),
+      ),
+    );
+  }
+}
